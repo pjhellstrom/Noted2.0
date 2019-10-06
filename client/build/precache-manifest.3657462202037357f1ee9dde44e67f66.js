a23435570bb7d5a858cbb3db5d1de685a0b8269f
pjhellstrom <pjh.hellstrom@gmail.com>
2019-10-06 14:01:15 -0400

    Added node engine config  to package.json

---
+++ client/build/precache-manifest.3657462202037357f1ee9dde44e67f66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1c1619b1fdd1e7b0a359ea025ca4e77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8620b030adbea6b268a8",
+    "url": "/static/css/main.5210b092.chunk.css"
+  },
+  {
+    "revision": "41247599483ecbb1be6c",
+    "url": "/static/js/2.89fe6b36.chunk.js"
+  },
+  {
+    "revision": "8620b030adbea6b268a8",
+    "url": "/static/js/main.6b0fc7e5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "019d85414812e186873f7993be683efc",
+    "url": "/static/media/notedlogo_md.019d8541.png"
+  },
+  {
+    "revision": "7b32b7b8c49aa9f4378dbfc852f1a86b",
+    "url": "/static/media/spinner.7b32b7b8.gif"
+  }
+]);
